5445376a8330434f1429f37479aec8b76b2789d5
eatingfoil@gmail.com <eatingfoil@gmail.com>
2018-05-15 01:48:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var arr = [];
   for (let i = 0; i < arr.length; i++) {
-    let str = `${musicians[i]} plays ${instruments[i]}`;
+    var str = `${musicians[i]} plays ${instruments[i]}`;
     arr.push(str);
   }
   return arr
